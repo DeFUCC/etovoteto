5ad121f1260a82533c1bb0858007e3c320bf1189
DeFUCC <fond@fondfrukt.ru>
2019-03-18 23:40:04 +0300

    Adding drafts works!

--- views.js
+++ views.js
@@ -73,18 +73,37 @@ const WordsPage = Vue.component('words', {
       newStep:1,
       valid:false,
       validDesc:true,
+      validAuthor:false,
       add: {
         word: '',
         stress:0,
         desc:''
       },
-      descRules: [
-        v => v.length<140 || 'Слишком длинное определение',
-        v => v.length>7 || 'Слишком короткое определение'
-      ],
-      wordRules:[
-        v => /^[А-Яа-яёЁ]*$/.test(v) || 'Только русские буквы',
-        v => (v || '').indexOf(' ') < 0 || 'Только одно слово, без пробелов'
+      author: {
+        name:'',
+        net:'instagram',
+        account:''
+      },
+      rules: {
+        name: [
+          v => !!v || 'Представьтесь, пожалуйста'
+        ],
+        account: [
+          v => /[a-zA-Z0-9_.]*/.test(v) || 'Таких аккаунтов не бывает'
+        ],
+        desc: [
+          v => v.length<140 || 'Слишком длинное определение',
+          v => v.length>7 || 'Слишком короткое определение'
+        ],
+        word: [
+          v => /^[А-Яа-яёЁ]*$/.test(v) || 'Только русские буквы',
+          v => (v || '').indexOf(' ') < 0 || 'Только одно слово, без пробелов'
+        ]
+      },
+      nets: [
+        {name:'instagram', url:'https://instagram.com/'},
+        {name:'vk', url:'https://vk.com/'},
+        {name:'facebook', url:'https://facebook.com/'}
       ],
     }
   },
@@ -94,10 +113,12 @@ const WordsPage = Vue.component('words', {
       this.newStep=1;
       this.stress=0;
     },
-    search(word) {
+    search(word, limit=30) {
       this.resetSearch();
 
       client.getItems('words',{
+        status: 'published',
+        limit:limit,
         filter: {
           word: {
             contains: word
@@ -112,11 +133,82 @@ const WordsPage = Vue.component('words', {
       }).catch(error => console.log(error));
     },
 
+    create(wordObj,authorObj) {
+      let {name,account,net} = authorObj;
+      let authorId;
+      console.log(authorObj)
+      if (name) {
+        client.createItem('authors',{
+          name:name,
+          status:'draft',
+          net:net,
+          account:account
+        }).then(
+          data => {
+            console.log(data)
+            authorId=data.data.id;
+            this.createPair(wordObj,authorId)
+          }
+        ).catch(e=>console.log(e));
+      } else {
+        this.createPair(wordObj);
+      }
+
+    },
+    createPair(wordObj,authorId=null) {
+      let {word,stress,desc} = wordObj;
+      word = word[0].toUpperCase() + word.substring(1);
+      let descId, wordId;
+
+      let request = client.createItem('desc', {
+        text:desc,
+        author:authorId,
+        status:'draft'
+      }).then(descData => {
+          console.log('desc',descData);
+          descId=descData.data.id
+
+          return client.createItem('words',{
+            status: 'draft',
+            word: word,
+            author:authorId,
+            stress: stress,
+            primary_desc:descId,
+          })
+      }).then(wordData => {
+          console.log('word',wordData)
+          wordId=wordData.data.id
+
+          return client.createItem('words_desc', {
+            words_id:wordId,
+            desc_id:descId
+          })
+      }).then(linkData => {
+            console.log('word-desc',linkData)
+
+            return client.createItem('authors_desc', {
+              authors_id:authorId,
+              desc_id:descId
+            })
+      }).then(linkData => {
+            console.log('authors-desc',linkData)
+
+            return client.createItem('authors_words', {
+              authors_id:authorId,
+              words_id:wordId
+            })
+      }).then(linkData => {
+        console.log('success!',linkData)
+      })
+
+
+    },
 
 
     getWords(limit=100) {
       client.getItems('words',{
-        fields:'word,stress,primary_desc.text,author.*,author.avatar.*.*',
+        fields:'word,stress,primary_desc.text,author.name, author.net, author.account',
+        status:'published',
         sort:'?',
         limit:limit
       }).then(data => {
@@ -130,9 +222,7 @@ const WordsPage = Vue.component('words', {
     parts() {
       if (!this.add.word) {this.add.word=''}
       let stress=this.add.stress-1;
-      console.log(stress)
       let arr = this.add.word.split('');
-      console.log(arr)
       let parts=[];
       if (arr.length>0) {
         arr[0] = arr[0].toUpperCase();
@@ -167,7 +257,7 @@ const DescPage = Vue.component('descs', {
   },
   created() {
     client.getItems('desc',{
-      fields:'id,text,author.name,author.link',
+      fields:'id,text,author.name,author.account,author.net',
       limit:100
     }).then(data => {
       console.log(data)
